73249621b3dde26b8f9b858c50177d1d1a931d31
stanleychow <chowtech1022@gmail.com>
2020-10-11 15:19:25 -0400

    updated get_file_lines

--- poetryslam.py
+++ poetryslam.py
@@ -1,11 +1,7 @@
-# def get_file_lines(filename):
-    # with open(filename) as f:
-    #     content = f.readlines()
-    #     content = [x.strip() for x in content]
 lines = []
+
 def get_file_lines(filename):
     with open(filename) as f:
-        # lines = []
         for line in f:
             lines.append(line)
         return lines
@@ -13,13 +9,21 @@ def get_file_lines(filename):
 get_file_lines('/Users/stanleychow/Poetry Slam/poem.txt')
 
 def lines_printed_backwards(lines_list):
+    textfile = open(lines_list)
+    lines = textfile.readlines()
+    for line in reversed(lines):
+        print(line)
+    textfile.close()
+
+lines_printed_backwards('/Users/stanleychow/Poetry Slam/poem.txt')
+    
 # (lines.reverse())
 # for i , item in enumerate(lines, start = 1):
 #     print(i, item)
-enumerate_lines = enumerate(lines, 1)
-enumerate_lines_list = list(enumerate_lines)
-# enumerate_lines.reverse()
-print(enumerate_lines_list[::-1])
+# enumerate_lines = enumerate(lines, 1)
+# enumerate_lines_list = list(enumerate_lines)
+# # enumerate_lines.reverse()
+# print(enumerate_lines_list[::-1])
 
 # def lines_printed_backwards(lines_list):
 #     file = open()
