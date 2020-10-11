5bd1c35ca17779c1dcf2db0449592d4ca631b2bb
stanleychow <chowtech1022@gmail.com>
2020-10-11 09:22:21 -0400

    Updated functions

--- poetryslam.py
+++ poetryslam.py
@@ -1,17 +1,36 @@
+# def get_file_lines(filename):
+    # with open(filename) as f:
+    #     content = f.readlines()
+    #     content = [x.strip() for x in content]
+lines = []
 def get_file_lines(filename):
-    file = open(filename)
-    lines = file.readlines()
-    for line in lines:
-        return(line)
-    file.close()
+    with open(filename) as f:
+        # lines = []
+        for line in f:
+            lines.append(line)
+        return lines
+
+get_file_lines('/Users/stanleychow/Poetry Slam/poem.txt')
 
 def lines_printed_backwards(lines_list):
-    file = open()
-    file.close()
+# (lines.reverse())
+# for i , item in enumerate(lines, start = 1):
+#     print(i, item)
+enumerate_lines = enumerate(lines, 1)
+enumerate_lines_list = list(enumerate_lines)
+# enumerate_lines.reverse()
+print(enumerate_lines_list[::-1])
 
-def lines_printed_random(lines_list):
+# def lines_printed_backwards(lines_list):
+#     file = open()
+#     file.close()
 
-def lines_printed_custom(lines_list):
+# def lines_printed_random(lines_list):
+
+# def lines_printed_custom(lines_list):
     
+# get_file_lines('./words.txt')
 
 
+
+# print(lines)
