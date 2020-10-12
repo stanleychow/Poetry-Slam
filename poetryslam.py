b29709f9f279b2c8a7bdd5e1fa52b23b0647cd88
stanleychow <chowtech1022@gmail.com>
2020-10-11 20:28:27 -0400

    changed reverse function and removed comments

--- poetryslam.py
+++ poetryslam.py
@@ -11,35 +11,11 @@ def get_file_lines(filename):
 get_file_lines('/Users/stanleychow/Poetry Slam/poem.txt')
 
 def lines_printed_backwards(lines_list):
-    textfile = open(lines_list)
-    lines = textfile.readlines()
-    num_list = [len(lines_list)]
-    for line in reversed(lines):
-        for num in num_list:
-            print(num)
-        print(line)
-    textfile.close()
+    (lines.reverse())
+    for i , item in enumerate(lines, start = 1):
+        print(i, item)
+    
+
 
 lines_printed_backwards('/Users/stanleychow/Poetry Slam/poem.txt')
     
-# (lines.reverse())
-# for i , item in enumerate(lines, start = 1):
-#     print(i, item)
-# enumerate_lines = enumerate(lines, 1)
-# enumerate_lines_list = list(enumerate_lines)
-# # enumerate_lines.reverse()
-# print(enumerate_lines_list[::-1])
-
-# def lines_printed_backwards(lines_list):
-#     file = open()
-#     file.close()
-
-# def lines_printed_random(lines_list):
-
-# def lines_printed_custom(lines_list):
-    
-# get_file_lines('./words.txt')
-
-
-
-# print(lines)
